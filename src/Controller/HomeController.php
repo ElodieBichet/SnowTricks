44fa3b17fecb8f08213e714c60ea40d7763a8d09
Elodie Bichet <e_bichet@yahoo.fr>
2021-05-04 15:42:00 +0200

    Generate Trick and Group controllers + TrickType

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TrickRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -11,8 +12,12 @@ class HomeController extends AbstractController
   /**
    * @Route("/", name="homepage")
    */
-  public function homepage()
+  public function homepage(TrickRepository $tricksRepository)
   {
-    return $this->render('home.html.twig');
+    $tricks = $tricksRepository->findBy([], [], 15);
+
+    return $this->render('home.html.twig', [
+      'tricks' => $tricks
+    ]);
   }
 }
