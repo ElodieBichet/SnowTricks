dc696cba533e42fb0beabc39887b3971aadd5f96
Elodie Bichet <e_bichet@yahoo.fr>
2021-05-06 22:15:54 +0200

    Add basic pagination for tricks on homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\TrickRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -12,9 +14,15 @@ class HomeController extends AbstractController
   /**
    * @Route("/", name="homepage")
    */
-  public function homepage(TrickRepository $tricksRepository)
+  public function homepage(TrickRepository $tricksRepository, Request $request, PaginatorInterface $paginator)
   {
-    $tricks = $tricksRepository->findBy([], ['updatedAt' => 'DESC'], 12);
+    $data = $tricksRepository->findBy([], ['updatedAt' => 'DESC']);
+
+    $tricks = $paginator->paginate(
+      $data,
+      $request->query->getInt('page', 1),
+      12
+    );
 
     return $this->render('home.html.twig', [
       'tricks' => $tricks
