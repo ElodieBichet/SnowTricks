f4992acb301b0d32e620ed67116506b17bdcb296
Elodie Bichet <e_bichet@yahoo.fr>
2021-05-20 19:01:17 +0200

    Remove KNP Paginator bundle

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,8 +3,6 @@
 namespace App\Controller;
 
 use App\Repository\TrickRepository;
-use Knp\Component\Pager\Event\PaginationEvent;
-use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
